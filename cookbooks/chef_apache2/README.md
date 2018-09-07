b6d71d347225c359aa59274be956a9f5cd72a559
Subrata Bauri <subrata@digitalaptech.com>
2018-09-07 14:38:34 +0000

    add new cookbook with Chef Generated Cookbook

---
+++ cookbooks/chef_apache2/README.md
@@ -0,0 +1,4 @@
+# chef_apache2
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/chef_apache2/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: chef_apache2
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
